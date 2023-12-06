7e7774eb8acc327513cba8c1f500e2090a778c92
Juanpaberna <bernabe.juanpablo2020@gmail.com>
2023-12-06 12:25:58 -0300

    agregue cosas

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const zapatilla = {
+    nombre: "NE"
+}
+
+//const contenedorFavoritos = document.querySelector(".main_container_faborites");
+//const contenedorFavoritos3 = document.getElementById("contenedor_favoritos");
+
+const zapatillas = document.querySelector(".zapas")
+zapatillas.innerHTML = `<h1>Holis</h1>`
